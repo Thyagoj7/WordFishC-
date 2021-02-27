d0635e061b32680cf90c57daa9aecf258c8ec576
Thyago <t.y_josef7@hotmail.com>
2021-02-27 16:33:37 -0300

    primiro commit

---
+++ WordFish/WordFish/Models/Context.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WordFish.Models
+{
+    
+        public class Context : DbContext
+        {
+            public DbSet<Fish> Fish{ get; set; }
+
+            
+
+            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            {
+                optionsBuilder.UseSqlServer(connectionString: @"Server=(localdb)\mssqllocaldb;Database=Cursomvc;Integrated Security=True");
+            }
+        }
+
+    }
+
